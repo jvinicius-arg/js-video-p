1feaa8c29f32e26256978d2a0050bdd4ced503a2
joão vinícius <j.vin7cius@gmail.com>
2022-03-14 22:16:31 -0300

    Implementações no player

---
+++ scripts/functions.js
@@ -0,0 +1,114 @@
+//buffered
+//played
+
+function play () {
+    let playP = document.querySelector(".play-ico");
+
+    video.addEventListener("click", play);
+    video.addEventListener("mouseover", () => playG.style.opacity = "0.7");
+    video.addEventListener("mouseout", () => playG.style.opacity = "0");
+    playG.addEventListener("mouseover", () => playG.style.opacity = "0.7");
+    playG.addEventListener("mouseout", () => playG.style.opacity = "0");
+
+    // Sincronização de efeitos de opacidade do botão principal com o controle;
+
+    if (playG.getAttribute("src") == "./assets/images/buttons/play.png") {
+        video.play();
+        var interval = setInterval(timer, 900);
+        playG.setAttribute("src", "./assets/images/buttons/pause.png");
+    } else {
+        video.pause();
+        clearInterval(interval);
+        playG.setAttribute("src", "./assets/images/buttons/play.png")
+    }
+    
+    if (playP.getAttribute("src") == "./assets/images/buttons/play-p.jpg") {
+        video.play();
+        var interval = setInterval(timer, 900);
+        playP.setAttribute("src", "./assets/images/buttons/pause-p.png");
+    } else {
+        video.pause();
+        clearInterval(interval);
+        playP.setAttribute("src", "./assets/images/buttons/play-p.jpg");
+    }
+
+    // Funcionalidades de play, pause e troca de estilos quando pressionados;
+}
+
+function timer () {
+    let timeContainer = document.querySelector("#time");
+    let duration = (video.duration / 60).toFixed(2);
+    let percentMin;
+    let seconds;
+
+    duration.slice(-2) <= 9 ? percentMin = duration.slice(-1) : percentMin = duration.slice(-2);
+    
+    percentMin = "0." + percentMin;
+    seconds = (parseFloat(percentMin) * 60).toFixed();
+    duration = duration.slice(0, 1) + ":" + seconds;
+
+    // Exibição da duração total;
+
+    let min = Math.floor(video.currentTime / 60);
+    console.log(min)
+    let sec = (video.currentTime).toFixed()
+
+    var parseSec = min * 60;
+
+    if (sec <= 9) {
+        sec = "0" + sec;
+    } else if (sec >= 60) {
+        sec -= parseSec;
+        if (sec <= 9) {
+            sec = "0" + sec;
+        }
+    }
+    
+    timeContainer.innerHTML = min + ":" + sec + "/" + duration;
+
+    if (video.ended == true) {
+        timeContainer.innerHTML = duration + "/" + duration;
+    }
+}
+
+function mute () {
+    let audio = document.querySelector(".audio-ico");
+
+    if (audio.getAttribute("src") == "./assets/images/buttons/audio-on.jpg") {
+        video.muted = true;
+        audio.setAttribute("src", "./assets/images/buttons/muted.jpg");
+    } else {
+        video.muted = false;
+        audio.setAttribute("src", "./assets/images/buttons/audio-on.jpg");
+    }
+
+    // Troca de estilos;
+}
+
+function back () {
+    let back = document.querySelector(".back-ico");
+
+    video.currentTime -= 10;
+    back.style.opacity = 0.5;
+    setTimeout(() => back.style.opacity = 1, 100);
+
+    // Volta 10s;
+}
+
+function forward () {
+    let forward = document.querySelector(".fwd-ico");
+
+    video.currentTime += 10;
+    forward.style.opacity = 0.5
+    setTimeout(() => forward.style.opacity = 1, 100);
+
+    // Avança 10s;
+}
+
+function fullScreen () {
+
+}
+
+function ended () {
+    video.style.filter = "saturate(0.2)";
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,25 @@
 let btnSendVideo = document.querySelector("#btn-1");
-let btnExp = document.querySelector("#btn-2");
-let videoContainer = document.querySelector(".btn-container");
 let playG;
 let progressBar;
 let progressTrack;
 let progressThumb;
+var duration;
+let seconds = 0;
 
-function showVideo () {
-    videoContainer.outerHTML = 
-    `<section class="video-container">
-        <video id="video" src="./assets/videos/experimental.mp4"></video>
-        <img class="play-center-ico center-btn" onclick="play()" src="./assets/images/buttons/play.png">
+progressBar = document.querySelector("#progress-bar");
+progressTrack = document.querySelector("#progress-bar-track");
+progressThumb = document.querySelector("#progress-bar-thumb");
 
-        <div id="controls">
-            <div id="progress-bar">
-                <div id="progress-bar-track">
-                </div>
-                <div id="progress-bar-thumb">
-                </div>
-            </div>
-            <img class="play-ico" onclick="play()" src="./assets/images/buttons/play-p.jpg">
-            <img class="back-ico" src="./assets/images/buttons/back.jpg">
-            <img class="fwd-ico" src="./assets/images/buttons/forward.jpg">
-            <img class="audio-ico" onclick="mute()" src="./assets/images/buttons/audio-on.jpg">
-            <img class="fscreen-ico" src="./assets/images/buttons/fullscreen.jpg">
-        </div>
-    </section>`;
+// Atribuições para montagem da barra de progresso;
 
-    // Atribuição de código HTML à página principal;
+playG = document.querySelector(".play-center-ico");
 
-    progressBar = document.querySelector("#progress-bar");
-    progressTrack = document.querySelector("#progress-bar-track");
-    progressThumb = document.querySelector("#progress-bar-thumb");
+let video = document.querySelector("#video");
+let controls = document.querySelector("#controls");
 
-    // Atribuições para montagem da barra de progresso;
-
-    playG = document.querySelector(".play-center-ico");
-
-    let video = document.querySelector("#video");
-    let controls = document.querySelector("#controls");
-
+if (!video) {
+    console.log("Vídeo não encontrado.")
+} else {
     video.addEventListener("mouseover", () => controls.style.opacity = "1");
     video.addEventListener("mouseout", () => controls.style.opacity = "0");
     controls.addEventListener("mouseover", () => controls.style.opacity = "1");
@@ -48,44 +28,25 @@ function showVideo () {
     playG.addEventListener("mouseout", () => controls.style.opacity = "0");
 
     // Eventos para inserção de opacidade no controle;
-}
 
-function play () {
-    let playP = document.querySelector(".play-ico");
+    let timeContainer = document.querySelector("#time");
+    video.onloadedmetadata = function () {
+        let duration = (this.duration / 60).toFixed(2);
+        let percentMin;
+        let seconds;
 
-    video.addEventListener("mouseover", () => playG.style.opacity = "0.7");
-    video.addEventListener("mouseout", () => playG.style.opacity = "0");
-    playG.addEventListener("mouseover", () => playG.style.opacity = "0.7");
-    playG.addEventListener("mouseout", () => playG.style.opacity = "0");
-
-    // Sincronização de efeitos de opacidade do botão principal com o controle;
-
-    if (playG.getAttribute("src") == "./assets/images/buttons/play.png") {
-        playG.setAttribute("src", "./assets/images/buttons/pause.png");
-    } else {
-        playG.setAttribute("src", "./assets/images/buttons/play.png")
-    }
+        duration.slice(-2) <= 9 ? percentMin = duration.slice(-1) : percentMin = duration.slice(-2);
+        
+        percentMin = "0." + percentMin;
+        seconds = (parseFloat(percentMin) * 60).toFixed();
+        duration = duration.slice(0, 1) + ":" + seconds;
+        
+        timeContainer.innerHTML = "0:00/" + duration;
+    } 
     
-    if (playP.getAttribute("src") == "./assets/images/buttons/play-p.jpg") {
-        playP.setAttribute("src", "./assets/images/buttons/pause-p.png");
-    } else {
-        playP.setAttribute("src", "./assets/images/buttons/play-p.jpg");
-    }
-
-    // Troca de estilos;
+    // Exibir timer
+    
+    progressTrack // qnt já viu
+    progressThumb // a bolinha
+    //pra amh
 }
-
-function mute () {
-    let audio = document.querySelector(".audio-ico");
-
-    if (audio.getAttribute("src") == "./assets/images/buttons/audio-on.jpg") {
-        audio.setAttribute("src", "./assets/images/buttons/muted.jpg");
-    } else {
-        audio.setAttribute("src", "./assets/images/buttons/audio-on.jpg");
-    }
-
-    // Troca de estilos;
-}
-
-// btnSendVideo.addEventListener("click", selectFile);
-btnExp.addEventListener("click", showVideo);
